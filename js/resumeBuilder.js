80bf4be6cab74f25e7e8179990f726f940caa4d3
Benjamin CANDELON <b.candelon@gmail.com>
2015-09-22 19:22:25 +0200

    test

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,16 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
- */
+*/
+var name = "Benjamin CANDELON, <i>Ph. D.</i>";
+var formatedname = HTMLheaderName.replace("%data%", name);
+
+var role = "IT Project Manager";
+var formatedrole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").prepend(formatedrole);
+$("#header").prepend(formatedname);
+
+var email = "b.candelon@gmail.com";
+
+var skills = ["Web", "Architecture", "Database", "Tools"];
+$("#main").append(skills);
