9a59735c11faefa4a8a14a086ec872e65b79b916
Benjamin CANDELON <b.candelon@gmail.com>
2015-09-22 20:00:49 +0200

    REvert to orginal CSS, Add object array to the builder

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -11,14 +11,33 @@ $("#header").prepend(formated_role);
 $("#header").prepend(formated_name);
 
 var address = " 78 rue de Mulhouse - 68300 SAINT-LOUIS - FRANCE"
-var cell_phone = "+33 6 50 71 80 40";
-var email = "b.candelon@gmail.com";
+var formated_address = HTMLcontactGeneric.replace("%contact%", "Address");
+var formated_address = formated_address.replace("%data%", address);
 
-var contactinfo = [address, cell_phone, email];
+var cell_phone = "+33 6 50 71 80 40";
+var formated_cell_phone = HTMLmobile.replace("%data%", cell_phone);
+
+var email = "b.candelon@gmail.com";
+var formated_email = HTMLemail.replace("%data%", email);
+
+var photo_url = "https://github.com/bcandelon/frontend-nanodegree-resume/blob/master/images/Ben_crop_small.jpg";
+
+var contactinfo = [formated_address, formated_cell_phone, formated_email];
+
+for (i = 0; i < contactinfo.length; i++) {
+	$("#topContacts").append(contactinfo[i]);
+}
+
+
+var welcome = "Coucou";
 var skills = ["Web", "Architecture", "Database", "Tools"];
 
 var bio = {
-	name,
-	role,
-	contactinfo	
+	"name" : name,
+	"role" : role,
+	"contactinfo" : contactinfo,
+	"photo_url" : photo_url,
+	"welcome" : welcome,
+	"skills" : skills
 }
+
